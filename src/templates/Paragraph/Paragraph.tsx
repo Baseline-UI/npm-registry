bf3016bce08a8f367c420474525b1ce5dd548cc8
Paxz <juliusberggrenkall@hotmail.com>
2024-07-07 00:35:24 +0200

    push update and testing
    new component testing

---
+++ src/templates/Paragraph/Paragraph.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+interface ParagraphProps {
+  // Add props here
+}
+
+export const Paragraph: React.FC<ParagraphProps> = (props) => {
+  return (
+    <p>Goodbye World!</p>
+  );
+};
---
+++ src/templates/Title/Title.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export const Title = ({
+
+}) => {
+	return (
+        <h1>Hello World!</h1>
+	);
+};
